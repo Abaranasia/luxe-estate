6d5d619416ae3c1c31e2bde0069c611bbb696120
Abaranasia <rankirlian@gmail.com>
2026-06-26 11:29:14 +0200

    chore: update to set turbopack and define image remotePatterns

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,36 @@
 import type { NextConfig } from "next";
+import path from 'path';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  turbopack: {
+    // We set the root to the directory where next.config.ts is located (__dirname)
+    root: path.join(__dirname),
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+      },
+      {
+        protocol: "https",
+        hostname: "plus.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.pixabay.com",
+      }
+    ],
+  },
 };
 
 export default nextConfig;
